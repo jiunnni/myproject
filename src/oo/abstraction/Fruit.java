a32e5fa3dcce4a991a42b83542b0f7aa2a61c5d3
Annie <Annie@Annie>
2017-03-10 00:37:02 +0800

    lalala

---
+++ src/oo/abstraction/Fruit.java
@@ -0,0 +1,8 @@
+package oo.abstraction;
+
+public class Fruit {
+	String name;
+	String shape;
+	int price;
+	String color;
+}
---
+++ src/oo/abstraction/FruitTester.java
@@ -0,0 +1,8 @@
+package oo.abstraction;
+
+public class FruitTester {
+
+	public static void main(String[] args) {
+		Fruit f1=new Fruit();
+	}
+}
