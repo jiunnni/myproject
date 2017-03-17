a6f63f560310a9725c4aeb884e1becca3324d64e
jiunnni <ya520709@gmail.com>
2017-03-17 19:46:10 +0800

    Update FruitTester.java

--- src/oo/abstraction/FruitTester.java
+++ src/oo/abstraction/FruitTester.java
@@ -3,6 +3,8 @@ package oo.abstraction;
 public class FruitTester {
 
 	public static void main(String[] args) {
-		Fruit f1=new Fruit();
+		Fruit f1=new Fruit("Grape","Circle",150,"Purple");
+		Fruit f2=new Fruit("Banana","Strip",100,"Yellow");
+		Fruit f3=new Fruit("Kiwi","Oval",200,"Brown");
 	}
 }
