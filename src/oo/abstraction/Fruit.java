c2ec34ee13e385a65f8d1e2806b23de5ba255966
jiunnni <ya520709@gmail.com>
2017-03-17 19:45:36 +0800

    Update Fruit.java

--- src/oo/abstraction/Fruit.java
+++ src/oo/abstraction/Fruit.java
@@ -5,4 +5,10 @@ public class Fruit {
 	String shape;
 	int price;
 	String color;
+	public Fruit (String name, String shape,int price,String color){
+		this.name=name;
+		this.shape=shape;
+		this.price=price;
+		this.color=color;
+	}
 }
