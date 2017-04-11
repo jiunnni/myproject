8be8e3695910efa5c20e8621a72c2e742509339b
user <user@es302n27>
2017-04-11 16:31:33 +0800

    111

---
+++ src/oo/stati/Tester.java
@@ -0,0 +1,15 @@
+package oo.stati;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Student s1= new Student(58,98,66);
+		Student s2= new Student(89,56,77);
+		s1.pass=70;
+		s1.print();
+		s2.print();
+		
+
+	}
+
+}
